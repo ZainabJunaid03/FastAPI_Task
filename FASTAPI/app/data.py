e8a7eca5cf1763505aef886208595543a382cb44
ZainabJunaid03 <zainabjunaid2003@gmail.com>
2025-01-24 10:29:20 +0500

    Delete FASTAPI directory

--- FASTAPI/app/data.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def get_template():
-    with open("template.json", "r") as file:
-        return json.load(file)
-
-def fill_template(data: dict):
-    template = get_template()
-    return {"message": "Template filled", "data": data}
